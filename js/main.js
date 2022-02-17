47f24071606b9e490ff09b44aec5fe307be59531
Jung HwiHak <70425723+Jung-Hwi-Hak@users.noreply.github.com>
2022-02-18 00:46:39 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,15 +1,53 @@
-$(document).ready(() => { 
-  badgeAnimate();
-  visualImg();
-  swiperF();
-  swiperS();
-  promotion();
+var pcEl = window.matchMedia("screen and (min-width:1280px)")
+var tabEl = window.matchMedia("screen and (min-width:768px) and (max-width: 1279px)")
+var mobEl = window.matchMedia("screen and (max-width:768px)");
+
+window.addEventListener('orientationchange', function () { 
+  location.reload();      
 });
+if (pcEl.matches) {
+  $(document).ready(() => {
+    badgeAnimate();
+    visualImg();
+    swiperF();
+    swiperS();
+    swiperFooter();
+    promotion();
+    scrollAnimate();
+    findYear();
+    toTop();
+  });
+} else if (tabEl.matches) {
+  $(document).ready(() => {
+    badgeAnimate();
+    visualImg();
+    swiperF();
+    swiperS();
+    swiperFooter();
+    promotion();
+    scrollAnimate();
+    findYear();
+    toTop();
+  });
+} else if (mobEl.matches) { 
+  $(document).ready(() => {
+    // badgeAnimate();
+    visualImg();
+    swiperF();
+    mobswiperS();
+    mobswiperFooter();
+    promotion();
+    scrollAnimate();
+    findYear();
+    // toTop();
+    toggleMenu();
+  });  
+}
 // !badge animate
 function badgeAnimate() {
   var $badges = $('.badges');
   window.addEventListener('scroll', _.throttle(function () {
-    if(window.scrollY > 500 ){
+    if(window.scrollY > 400 ){
       gsap.to($badges, 0.5, {
         opacity: 0,
         display: 'none',
@@ -37,14 +75,15 @@ function visualImg() {
 function swiperF() {
   new Swiper(" section:nth-of-type(2) > div:nth-of-type(1) .swiper-container", {
     direction: "vertical",
+    slidesPerView: 1,
     autoplay: true,
     loop: true,
   });
 }
+
 // ! section:nth-of-type(2) div:nth-of-type(2)
 function swiperS() {
   new Swiper('section:nth-of-type(2) > div:nth-of-type(2) .swiper-container', {
-    direction: 'horizontal',
     slidesPerView: 3,
     spaceBetween: 10,
     centeredSlides: true,
@@ -60,6 +99,51 @@ function swiperS() {
       prevEl: "section:nth-of-type(2) > div:nth-of-type(2) .swiper-prev",
       nextEl: "section:nth-of-type(2) > div:nth-of-type(2) .swiper-next",
     },
+  });
+}
+function mobswiperS() {
+  new Swiper('section:nth-of-type(2) > div:nth-of-type(2) .swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+    },
+    pagination: {
+      el: 'section:nth-of-type(2) > div:nth-of-type(2) .swiper-pagination',
+      clickable: true,
+    },
+  });
+}
+function swiperFooter() { 
+  new Swiper('footer .inner .swiper-container', {
+    direction: 'horizontal',
+    slidesPerView: 5,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    }
+    // navigation: {
+    //   prevEl: "footer .inner .swiper-container .swiper-prev",
+    //   nextEl: "footer .inner .swiper-container .swiper-next",
+    // },
+  });
+}
+function mobswiperFooter() { 
+  new Swiper('footer .inner .swiper-container', {
+    direction: 'horizontal',
+    slidesPerView: 2,
+    spaceBetween: 100,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    }
+    // navigation: {
+    //   prevEl: "footer .inner .swiper-container .swiper-prev",
+    //   nextEl: "footer .inner .swiper-container .swiper-next",
+    // },
   });
 }
 // ! promotion logic
@@ -84,9 +168,64 @@ function promotionToggle() {
     $promotionEl.removeClass('hide');
   }
 }
+// ! scroll animate
+function scrollAnimate() { 
+  var spyEl = $('section.scroll-spy');
+  spyEl.each(function (index, el) { 
+    new ScrollMagic
+      .Scene({
+        triggerElement: el,
+        triggerHook: 0.8,
+      })
+      .setClassToggle(el, 'show')
+      .addTo(new ScrollMagic.Controller());
+  });
 
+}
+// footer year auto change logic
+function findYear() { 
+  var thisYear = document.querySelector('.this-year');
+  thisYear.textContent = new Date().getFullYear();
+}
 
-// ! section-nth-of-type(4) .inner img
-function floating(selector) { 
-  
+var topBtn = $('.to-top');
+function toTop() { 
+  window.addEventListener('scroll', toTopScroll);
+  topBtn.click(topBtnAction);
+}
+function toTopScroll() { 
+  if (window.scrollY > 400) {
+    gsap.to(topBtn,0.5,{
+      opacity: 1,
+      right: 30,
+    })
+  } else { 
+    gsap.to(topBtn,0.5,{
+      opacity: 0,
+      right: -100,
+    })
+  }
+}
+function topBtnAction() { 
+  $('html,body').animate({ scrollTop: 0 }, 600);
+}
+// ! 메뉴 토글
+var $menuToggle = $('span.material-icons.menu');
+var $menuSlide = $('div.slide');
+var $menuX = $('div.slide span.material-icons');
+var toggle = false;
+function toggleMenu()
+{ 
+  $menuToggle.click(toggleAction);
+}
+function toggleAction()
+{
+  toggle = !toggle;
+  if (toggle) {
+    $menuSlide.css('right', 0);
+  }
+  $menuX.click(function () { 
+    $menuSlide.css('right', -9999);
+    toggle = !toggle;
+  });
 }
--- js/youtube.js
+++ js/youtube.js
@@ -1,18 +1,24 @@
-      var tag = document.createElement('script');
+var mobEl = window.matchMedia("screen and (max-width:768px)");      
 
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+if (mobEl.matches) {
+    console.log('mob');
+} else { 
+var tag = document.createElement('script');
 
-function onYouTubeIframeAPIReady() {
-    new YT.Player('player', {
-        videoId: '778JWi1rWuY',
-        playerVars: {
-            autoplay: true,
-            loop: true,
-            mute: true,
-            playlist: '778JWi1rWuY',
-            controls: 0,
-        },
-    });
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    function onYouTubeIframeAPIReady() {
+        new YT.Player('player', {
+            videoId: '778JWi1rWuY',
+            playerVars: {
+                autoplay: true,
+                loop: true,
+                mute: true,
+                playlist: '778JWi1rWuY',
+                controls: 0,
+                origin: 'http://127.0.0.1:5500',
+            },
+        });
+    }
 }
